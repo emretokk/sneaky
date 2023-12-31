e83288f5526b3bf8e8325388e7c954f2751263a0
emre <emretok35@gmail.com>
2023-12-31 18:17:57 +0300

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,70 +2,77 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full bg-slate-400 pt-2">
-      <div className="flex justify-around px-40 py-10 border-b border-black">
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold mb-2">Categories</h1>
+    <footer className="w-full bg-green-50 pt-2">
+      <div className="flex justify-around px-40 py-10 border-b border-gray-400">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-semibold mb-2">Kategoriler</h1>
           <a href="#" className="text-sm">
-            Casual
+            Klasik
           </a>
           <a href="#" className="text-sm">
-            Basketball
+            Basketbol
           </a>
           <a href="#" className="text-sm">
-            Football
+            Futbol
           </a>
           <a href="#" className="text-sm">
-            Running
-          </a>
-        </div>
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold mb-2">Brands</h1>
-          <a href="#" className="text-sm">
-            Casual
-          </a>
-          <a href="#" className="text-sm">
-            Basketball
-          </a>
-          <a href="#" className="text-sm">
-            Football
-          </a>
-          <a href="#" className="text-sm">
-            Running
+            Koşu
           </a>
         </div>
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold mb-2">Company</h1>
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-semibold mb-2">Markalar</h1>
           <a href="#" className="text-sm">
-            Casual
+            Nike
           </a>
           <a href="#" className="text-sm">
-            Basketball
+            Adidas
           </a>
           <a href="#" className="text-sm">
-            Football
+            Under Armour
           </a>
           <a href="#" className="text-sm">
-            Running
+            Puma
+          </a>
+          <a href="#" className="text-sm">
+            Converse
+          </a>
+          <a href="#" className="text-sm">
+            Reebok
           </a>
         </div>
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold mb-2">Social</h1>
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-semibold mb-2">Kurumsal</h1>
           <a href="#" className="text-sm">
-            Casual
+            Hakkımızda
           </a>
           <a href="#" className="text-sm">
-            Basketball
+            Franchise
           </a>
           <a href="#" className="text-sm">
-            Football
+            İletişim
+          </a>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-semibold mb-2">Sosyal Medya</h1>
+          <a href="#" className="text-sm">
+            İnstagram
+          </a>
+          <a href="#" className="text-sm">
+            Twitter
+          </a>
+          <a href="#" className="text-sm">
+            Tik Tok
           </a>
           <a href="#" className="text-sm">
             Running
           </a>
         </div>
       </div>
-      <div>copyright 2023</div>
+      <div className="flex p-1">
+        <p className="text-sm font-light ml-4">
+          ©2024 Copyright Tüm Hakları Saklıdır.
+        </p>
+      </div>
     </footer>
   );
 }
