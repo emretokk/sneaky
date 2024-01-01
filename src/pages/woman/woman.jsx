8a42e2ab97be5eaf66463c83b37464dfca187c2e
emre <emretok35@gmail.com>
2024-01-01 17:45:22 +0300

    urun listeleme template

--- src/pages/woman/woman.jsx
+++ src/pages/woman/woman.jsx
@@ -6,6 +6,23 @@ function Woman() {
   return (
     <div id="Woman">
       <Header />
+      {/* Content */}
+      <div>
+        <h1 className="mt-10 text-center font-semibold text-xl">Kadın</h1>
+        {/* listing area */}
+        <div className="p-12">
+          {/* filter order cart curt */}
+          <div className="flex bg-orange-200">
+            <p className="flex-none">31 ürün bulundu</p>
+            <div className="flex-auto flex flex-row-reverse">
+              <div>sırala</div>
+              <div>filtrele</div>
+            </div>
+          </div>
+          {/* products */}
+          <div className="h-32 w-full bg-green-700">asdfa</div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
